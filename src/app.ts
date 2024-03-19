a0aa295ac79fc2221c15c3693867b27caf1be596
ThomasCardiet <thom_car@yahoo.fr>
2024-03-19 15:08:41 +0100

    [feat/character-images-enpoint][feat]: add character images enpoint

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import fs from 'fs';
 import path from 'path';
 import countries from './countries';
 
@@ -125,6 +126,43 @@ app.get('/api/game/:uuid', async (req, res) => {
       message: 'Error fetching game by UUID.',
     });
   }
+});
+
+/**
+ * GET route to get all character url images.
+ *
+ * This route asynchronously retrieves all image URLs from the characters directory
+ * and returns them to the client.
+ *
+ * @param {Function} async (req, res) - The asynchronous route handler function.
+ */
+app.get('/api/character-images', async (req, res) => {
+  // Define the directory path for character images
+  const imagesDirectory = path.join(__dirname, '../public/images/characters');
+
+  // Read the directory contents
+  fs.readdir(imagesDirectory, (err, files) => {
+    if (err) {
+      // Log and return an error message if reading the directory fails
+      console.error('Error reading image directory:', err);
+      return res
+        .status(500)
+        .json({ message: 'Error reading image directory.' });
+    }
+
+    // Filter the list to include only image files based on their extensions
+    const imageFiles = files.filter((file) =>
+      /\.(jpg|jpeg|png|gif)$/i.test(file)
+    );
+
+    // Construct URLs for each image file
+    const urls = imageFiles.map(
+      (file) => `${req.protocol}://${req.get('host')}/images/characters/${file}`
+    );
+
+    // Return the list of image URLs to the client
+    res.json(urls);
+  });
 });
 
 app.listen(PORT, () => {
